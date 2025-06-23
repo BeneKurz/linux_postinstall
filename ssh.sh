9be0cdd727faec51b2446f22deb05bb1dc6cfdf9
Bene Kurz <benekurz@online.de>
2025-06-23 16:52:41 +0200

    ssh

---
+++ ssh.sh
@@ -0,0 +1,12 @@
+echo "Installiere ssh"
+sudo apt-get install openssh-server -y
+sudo systemctl enable ssh
+sudo systemctl enable ssh
+
+sudo service ssh status
+
+echo "Öffne FIrewall ssh"
+sudo ufw enable
+sudo ufw allow 22/tcp
+sudo ufw status verbose 
+
