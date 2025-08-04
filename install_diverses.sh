022adf2914e76e4135ffcdb8bbe6a9e2f649323f
Bene Kurz <benekurz@onlind.de>
2025-08-04 14:50:11 +0200

    anpassungen

--- install_diverses.sh
+++ install_diverses.sh
@@ -3,3 +3,5 @@ echo "installieren diverses ..."
 sudo apt install yadm tmux htop powertop telegram
 sudo apt install keepassxc
 sudo apt install strongswan strongswan-pki libcharon-extra-plugins
+sudo apt install xrdp
+sudo ufw allow 3389/tcp
--- install_x11vnc.sh
+++ install_x11vnc.sh
@@ -4,9 +4,11 @@ sudo apt-get -y remove vino
 sudo apt-get -y install x11vnc
 sudo mkdir /etc/x11vnc
 
+sudo ufw allow 5900
+
 sudo x11vnc --storepasswd /etc/x11vnc/vncpwd
 
-mv x11_service_template  /lib/systemd/system/x11vnc.service
+sudo mv x11_service_template  /lib/systemd/system/x11vnc.service
 
 sudo systemctl daemon-reload
 
