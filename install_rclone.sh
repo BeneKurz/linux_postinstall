2766c466851fe9eb20e429113615de06fc3645ff
Bene Kurz <benekurz@onlind.de>
2025-08-07 14:29:29 +0200

    rclone

--- install_rclone.sh
+++ install_rclone.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 echo "Installiere rclone"
-sudo apt install rclone rclone-browser
+#sudo apt install rclone rclone-browser
 
-
+RC_DEB="rcloneui.deb"
+wget -qO- https://get.rcloneui.com/linux-deb > $RC_DEB
+sudo dpkg -i $RC_DEB
+/bin/rm -f $RC_DEB
